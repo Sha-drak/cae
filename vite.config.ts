94321d55e043f35bd8c80ddb930c4d608480b68c
Shadrach Alale <sharonalale7@gmail.com>
2026-08-23 23:03:26 +0000

    Added favicon as app icon

--- vite.config.ts
+++ vite.config.ts
@@ -20,14 +20,15 @@ export default defineConfig({
         orientation: 'portrait-primary',
         icons: [
           {
-            src: '/favicon.svg',
-            sizes: 'any',
-            type: 'image/svg+xml'
-          },
-          {
             src: '/android-icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
+          },
+          {
+            src: '/pwa-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
           }
         ]
       }
